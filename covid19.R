a77fc4f1d9f01f77f1467f8222950037b1228e72
Morten Jagd Christensen <mortenjc@jcaps.com>
2020-03-23 08:26:16 +0100

    improvements, trimming excel file and reordering columns prior to write

--- covid19.R
+++ covid19.R
@@ -3,29 +3,41 @@
 if (!require("pacman")) install.packages("pacman")
 
 # Use pacman to load add-on packages as desired
-pacman::p_load(pacman, rJava, xlsx, dplyr) 
-
-# Read the covid excel sheet
-file = file.choose(new = FALSE)
-res <- read.xlsx(file, 1)
-# add new columns
-res$date <- 0
-res$aggcases <- 0
-res$aggdeaths <- 0
-res$region <- "na"
-
-# read the region database xlsx
-regres <- read.xlsx("names.xlsx", 1)
-
-#Get list of countries from data frame
-countries <- unique(res %>% select(Countries.and.territories))
-
-# uncomment if you want to generate new country list
-#country_file = file.choose(new = TRUE)
-#write.xlsx(countries, country_file, sheetName="countries")
+pacman::p_load(pacman, rJava, xlsx, httr, dplyr, readxl)
 
 
+# Read the covid excel sheet from file
+getexcelfromfile <- function() {
+  file = file.choose(new = FALSE)
+  res <- read.xlsx(file, 1)
+  # add new columns
+  res$aggcases <- 0
+  res$aggdeaths <- 0
+  res$region <- "na"
+  return(res)
+}
 
+# From ECDC should get excel from URL (currently doesn't work)
+getexcelfromurl <- function() {
+  #create the URL where the dataset is stored with automatic updates every day
+  url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
+  
+  #download the dataset from the website to a local temporary file
+  GET(url, authenticate(":", ":", type="ntlm"), write_disk(tf <- "temp.xlsx"))
+  
+  #read the Dataset sheet into “R”
+  res <- read_excel(tf)
+  
+  # add new columns
+  res$aggcases <- 0
+  res$aggdeaths <- 0
+  res$region <- "na"
+  return(res)
+}
+
+
+# add the aggregated cases and deaths for a single country
+# by summation on date sorted data 
 sumcountry <- function(name) {
   country <- as.character(name)
   print(paste("name: ", name))
@@ -47,10 +59,34 @@ sumcountry <- function(name) {
     scdata[row, "aggcases"] <- sumcases
     scdata[row, "aggdeaths"] <- sumdeaths
   } # end loop dates
-  print(paste("added ", nrow(scdata), " rows"))
+  #print(paste("added ", nrow(scdata), " rows"))
   return (scdata)
 }
 
+
+# Select columns relevant for Gapminder
+trimdata <- function(df) {
+  #trimmed <- within(df, rm('Day, Month, Year, GeoId))
+  res <- subset(df, select=c('Countries.and.territories', 'DateRep', 'aggcases', 'aggdeaths', 'Cases', 'Deaths', 'region'))
+  return(res)
+}
+
+#
+# #
+#
+
+#res <- getexcelfromurl()
+res <- getexcelfromfile()
+
+# read the region database xlsx
+regres <- read.xlsx("names.xlsx", 1)
+
+#Get list of countries from data frame
+countries <- unique(res %>% select(Countries.and.territories))
+
+# uncomment if you want to generate new country list
+#country_file = file.choose(new = TRUE)
+#write.xlsx(countries, country_file, sheetName="countries")
 
 total <- sumcountry("Guernsey")
 tail(total)
@@ -62,14 +98,16 @@ for (row in 1:nrow(countries)) {
   total = rbind(total, tmp)
 }
 
+final <- trimdata(total)
+
 # write data to exel, for gapminder
-write.xlsx(total, "gap.xlsx", sheetName="countries")
+write.xlsx(final, "gap2.xlsx", sheetName="countries")
 
 
 # CLEAN UP #################################################
 
 # Clear environment
-rm(list = ls()) 
+rm(list = ls())
 
 # Clear packages
 p_unload(all)  # Remove all add-ons
@@ -82,4 +120,3 @@ dev.off()  # But only if there IS a plot
 cat("\014")  # ctrl+L
 
 # Clear mind :)
-
