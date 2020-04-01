a7475e0257fcefa5a3bae50537096d6347010b66
Morten Jagd Christensen <mortenjc@jcaps.com>
2020-04-01 13:19:59 +0200

    formatting

--- covid19.R
+++ covid19.R
@@ -7,34 +7,28 @@ if (!require("pacman")) install.packages("pacman")
 # Use pacman to load add-on packages as desired
 pacman::p_load(pacman, rJava, xlsx, httr, dplyr, ggplot2, cowplot, scales, REAT)
 
-source("ecdc_funcs.R") # functions to handle the ecdc dataset
-source("plot_funcs.R") # plotting funbctions (ggplot2 + cowplot)
+source("ecdc_funcs.R") # functions to handle the ECDC dataset
+source("plot_funcs.R") # plotting functions (ggplot2 + cowplot)
 
 ####### The fun starts below #######
 
 #
 res <- getexcelfromurl()
 
-#read the region database xlsx
+# read the region database xlsx
 regres <- read.xlsx("region_names.xlsx", 1)
 
-#Get list of countries from data frame
+# get list of countries from data frame
 countries <- unique(res %>% select(countriesAndTerritories))
 
-# generate new country list
+# write new country list
 write.xlsx(countries, "country_list.xlsx", sheetName="countries")
 
-
-
-
+# gapminder compatible data frame
 final <- gapminder(100)
 
 # write data to exel, for gapminder
 write.xlsx(final, "gapminder.xlsx", sheetName="countries", row.names=FALSE)
-
-
-
-
 multiplot(final)
 
 #cleanup()
